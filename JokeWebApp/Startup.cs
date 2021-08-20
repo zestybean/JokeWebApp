3494add3f1b43dbd5d3d47ad5eff900cc3571d01
brando.lugo <brando.lugo@CKB-OPS003>
2021-08-20 08:57:54 -0500

    Add project files.

---
+++ JokeWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JokeWebApp.Startup))]
+namespace JokeWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
